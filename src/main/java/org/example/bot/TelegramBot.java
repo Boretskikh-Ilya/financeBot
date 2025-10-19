b0c63683a6b26ff534179f1521abc9e089f462b6
SashaRey <aleksandrkatruhin6@gmail.com>
2025-10-19 20:50:41 +0500

    Initial commit

---
+++ src/main/java/org/example/bot/TelegramBot.java
@@ -0,0 +1,35 @@
+package org.example.bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotToken() {
+        return "7596704485:AAENl2PrL6D7Qxp4ilcQh9KLAR0VrDSXnsg";
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "finance_matmech_bot";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        String chatId = update.getMessage().getChatId().toString();
+        String text = update.getMessage().getText();
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(text);
+
+        try {
+            this.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
